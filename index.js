dc438c888a613f6948829260b33afb62958126f4
Steven Chen <ysteven.chen2@gmail.com>
2018-03-30 03:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function takeANumber(katzDeliLine,newName){
   }
 
 function nowServing(katzDeliLine){
-  katzDeliLine.shift();
-  return ("Currently serving "+katzDeliLine[0]+".")
+if(katzDeliLine.length=0){
+console.log"There is nobody waiting to be served!"
+}
+  return ("Currently serving "+katzDeliLine.shift()+".")
   }

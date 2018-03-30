184c603b58355b5616f8e256d811bfabac4664b6
Steven Chen <ysteven.chen2@gmail.com>
2018-03-30 02:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine,newName){
   for(var i=0;i<newName.length;i++){
     katzDeliLine.push(newName[i]);
-    return ("Welcome,"+newName[i]+". You are number "+newName.length-2+" in line.");
+    return ("Welcome,"+newName[i]+". You are number "+(newName.length-2)+" in line.");
   }
 }

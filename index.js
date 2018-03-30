c54d4179a5b8bd96df63689db07db83e544e2888
Steven Chen <ysteven.chen2@gmail.com>
2018-03-30 02:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber(katzDeliLine,newName){
-  for(var i=0;i<newName.length;i++){
-    katzDeliLine.push(newName[i]);
-    return ("Welcome, Ada. You are number "+(newName.length-2)+" in line.");
+    katzDeliLine.push(newName);
+    return ("Welcome,"+${newName}". You are number "+(katzDeliLine.length)+" in line.");
   }
 }

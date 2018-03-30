305e48ff52ce3c81eae238bba7924d031f2d79fb
Steven Chen <ysteven.chen2@gmail.com>
2018-03-30 03:27:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ return("There is nobody waiting to be served!")
 }
   return ("Currently serving "+katzDeliLine.shift()+".")
   }
+function currentLine(line){
+  if(line.length==0){
+    return ("The line is currently empty.");
+  }
+return ("The line is currently:"+line)
+}
